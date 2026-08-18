e7799047ab66b8a0b133d868b2ffd0e4fa120748
chenmanhjdx <chenmanhjdx@163.com>
2026-08-18 13:55:18 +0800

    Delete mmdet directory

--- mmdet/models/detectors/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import BaseDetector
-from .faster_rcnn import FasterRCNN
-from .rpn import RPN
-from .two_stage import TwoStageDetector
-
-__all__ = [
-    'BaseDetector', 'TwoStageDetector', 'RPN',
-    'FasterRCNN',
-]
--- mmdet/models/refiners/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cdip_miner import CDIPMiner
-from .sipb_optimizer import SIPBOptimizer
-
-__all__ = ['CDIPMiner', 'SIPBOptimizer']
